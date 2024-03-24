75fe7045f70c3fa5b897357aac5c46ad780dc9fd
rockingpenny4 <rockingpenny4@gmail.com>
2024-03-25 02:07:28 +0530

    fix typos

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,8 +4,8 @@
     <DarkModeToggle :darkMode="darkMode" @update:dark-mode="updateDarkMode" />
     <InfoIcon />
     <p>Wiki Word is a Vue JS app using the Opensearch API  and Codex UI library.
-         One can serach for an article and it shows another search field where one can select the article language they want to search in,
-         the word they want to serach for and the number of serach results to be shown(1-500). All this is clubbed with a dark mode functionality,
+         One can search for an article and it shows another search field where one can select the article language they want to search in,
+         the word they want to search for and the number of search results to be shown(1-500). All this is clubbed with a dark mode functionality,
          and an about page which you are seeing right now :P , click on the home icon to get back to the main page. Adios!
     </p>
   
